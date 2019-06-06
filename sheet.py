76835c2228d06cf6c99b3e307dcadf5c84c47ade
Maslianko Taras <tarasmaslianko@gmail.com>
2019-06-06 20:34:39 +0300

    created bot; basic linked with sheet

--- sheet.py
+++ sheet.py
@@ -1,10 +1,10 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-from database_interface import *
+from pprint import pprint
 
 
 class Sheet:
-    def __init__(self, name):
+    def __init__(self):
         self.scope = ["https://spreadsheets.google.com/feeds",
                     "https://www.googleapis.com/auth/spreadsheets",
                     "https://www.googleapis.com/auth/drive.file",
@@ -13,16 +13,11 @@ class Sheet:
         self.creds = ServiceAccountCredentials.\
             from_json_keyfile_name("PhotosVoteBot-b287f9a46ce7.json", self.scope)
         self.client = gspread.authorize(self.creds)
-        self.sheet = self.client.open(name).sheet1
+        self.sheet = self.client.open("photos_vote_bot").sheet1
 
-    def vote(self, user_id, place, number):
-        votes_number = int(self.sheet.cell(number + 1, place + 2).value)
-        self.sheet.update_cell(number + 1, place + 2, votes_number + 1)
-        vote(user_id, place, number)
-
-    def unvote(self, place, number):
-        votes_number = int(self.sheet.cell(number + 1, place + 2).value)
-        self.sheet.update_cell(number + 1, place + 2, votes_number - 1)
+    def vote(self, number, place):
+        votes_number = self.sheet.cell(number + 1, place + 2).value
+        self.sheet.update_cell(number + 1, place + 2, int(votes_number) + 1)
 
     def null(self):
         data = self.sheet.get_all_values()
@@ -36,3 +31,13 @@ class Sheet:
     def get_participants(self):
         return self.sheet.col_values(2)[1:]
 
+s = Sheet()
+print(s.get_participants())
+
+#i = 0
+
+#sheet.update_cell(2,2, i + 1)
+#i = sheet.cell(2,2).value
+#sheet.update_cell(2,2, int(i) + 2)
+
+#pprint(data[1]['ФИО'])
